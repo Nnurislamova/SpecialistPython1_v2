d2b8eb1331b81554c6095767edd0de493203bf64
Nnurislamova <114989817+Nnurislamova@users.noreply.github.com>
2022-10-06 15:48:34 +0300

    Update 02_task_func.py

--- Module6/practice/funcs_part1/02_task_func.py
+++ Module6/practice/funcs_part1/02_task_func.py
@@ -4,17 +4,14 @@
 # * попробуйте решить данную задачу не используя строки
 
 def palindrome(number):
-    pass
+    if (str(number)[::-1]) == (str(number)[::1]):
+        return "true"
+    else: return "false"
 
-
-# Тестируем функцию
 print(palindrome(3454))
 print(palindrome(3443))
 print(palindrome(1234541))
 print(palindrome(1234321))
 print(palindrome(77777))
 
-# Подсказка:
-# Самый простой способ решить задачу, работать с полученным числом как со строкой
-# Преобразование к строке:  str(1234) --> "1234"
-# Переворот строки:         "1234"[::-1] --> "4321"
+
